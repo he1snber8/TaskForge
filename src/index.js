6078c35543b8645a623d80e7a3ae92c69d0e8b9b
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-09-20 16:40:16 +0400

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// src/index.js
+const express = require("express");
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+app.listen(process.env.PORT||3000, ()=>console.log("API running"));
